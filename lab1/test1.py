cc34fa1fe646172a0c8d32cd57687b5e533cb2eb
Ironhide1234 <ironhide610@gmail.com>
2021-09-22 18:04:08 +0300

    new v

--- lab1/test1.py
+++ lab1/test1.py
@@ -2,12 +2,14 @@ from sys import argv
 
 userinp = argv[1:]
 
+signs = "+-*/"
+
 def answ(userinp):
-    if len(userinp) == 3:
+    if len(userinp) == 3 and userinp[1] in signs:   
         try:
-            return eval(''.join(userinp))        
+            return eval(''.join(userinp))   
         except:
-           return None
+            return None
     else:
         return None
 print(answ(userinp))
