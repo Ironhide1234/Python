4b571ec32db9b176cf0f1f48b99a5e12e72de1cf
Ironhide1234 <ironhide610@gmail.com>
2021-09-30 11:19:59 +0300

    Prac

---
+++ prac/task1.py
@@ -0,0 +1,24 @@
+class Rectangle:
+    def __init__(self,width = 1, height = 1):
+        self.setter(width,height)
+    def setter(self,width,height):
+        if h <= 0 and w <= 0:
+            print("Incorrect number")
+        else:
+            self.height  = height      
+            self.width = width
+    def perimeter(self):
+        return 2 * self.height + 2 * self.width
+    def area(self):
+        return self.width * self.height 
+    def getStats(self):
+        return "width:  %s\nheight:     %s\narea:      %s\nperimeter: %s" % (self.width, self.height, self.area(), self.perimeter())
+    
+    
+try:    
+    h = float(input())
+    w = float(input())
+    r = Rectangle(w,h)
+    print(r.getStats())      
+except:
+    print("Error1")        
