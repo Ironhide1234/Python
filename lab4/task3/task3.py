a63e3cbfa22c409b203517a327cf9974cee27213
Ironhide1234 <ironhide610@gmail.com>
2021-12-23 11:26:31 +0200

    added json but not used and removed interfaces to another file

--- lab4/task3/task3.py
+++ lab4/task3/task3.py
@@ -1,52 +1,8 @@
-from abc import ABC, abstractmethod
+import json
+from interface import ICourse, ILocalCourse, IOffsiteCourse, ITeacher, ICourseFactory
 
-class ICourseFactory(ABC):
-    @abstractmethod
-    def setSpecialty():
-        pass
-
-class ICourse(ICourseFactory):
-    @abstractmethod
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def setTeacher(self):
-        pass
-
-    @abstractmethod
-    def setPlan(self):
-        pass
-
-    @abstractmethod
-    def __str__(self):
-        pass
-
-class ITeacher(ICourseFactory):
-    @abstractmethod
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def setName(self):
-        pass
-
-    @abstractmethod
-    def __str__(self):
-        pass
-
-
-class ILocalCourse(ICourse):
-  @abstractmethod
-  def setAuditory(self):
-    pass
-
-
-class IOffsiteCourse(ICourse):
-  @abstractmethod
-  def setPlace(self):
-    pass
-
+COURSES = "courses.json"
+TEACHERS = "teachers.json"
     
 class Course(ICourseFactory):
     def __init__(self,specialization,teacher,plan):
